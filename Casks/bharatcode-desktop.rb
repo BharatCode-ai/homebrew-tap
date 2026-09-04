52d824b2b1e2de5a893cca8024bfe34d61063980
Shrey Gupta <shrey16.sg@gmail.com>
2026-09-04 18:35:17 +0530

    chore: update desktop cask to 1.15.25

--- Casks/bharatcode-desktop.rb
+++ Casks/bharatcode-desktop.rb
@@ -1,9 +1,9 @@
 cask "bharatcode-desktop" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.15.23,desktop-beta-1.15.23"
-  sha256 arm: "94e1eccd71dd9d88cf107b15f92aff28f97b8d63aef9d6f08400c40209099674",
-         intel: "98e0b94c004bbed59773ee71011c31f6c6b5d4f470ba5da34165f0316e78cc5b"
+  version "1.15.25,desktop-beta-1.15.25"
+  sha256 arm: "e4e44fa1c03be2a36f1ff1a3f19cc53ff5741b81a31a1e2a95a4f8aed90c5fe4",
+         intel: "3dd7c12679fafa1243a6a1920df958fcfea196a3ba334546d7982140cd227073"
 
   url "https://github.com/BharatCode-ai/bharatcode-desktop/releases/download/#{version.csv.second}/bharatcode-desktop-next-beta-mac-#{arch}.zip",
       verified: "github.com/BharatCode-ai/bharatcode-desktop/"
