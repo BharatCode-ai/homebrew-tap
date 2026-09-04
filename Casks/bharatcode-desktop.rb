2cad0ef07f337602e4617bfb9b93ab7395b751b7
Shrey Gupta <shrey16.sg@gmail.com>
2026-09-05 03:22:17 +0530

    chore(cask): prepare BharatCode Desktop 1.15.26

--- Casks/bharatcode-desktop.rb
+++ Casks/bharatcode-desktop.rb
@@ -1,9 +1,9 @@
 cask "bharatcode-desktop" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.15.25,desktop-beta-1.15.25"
-  sha256 arm: "e4e44fa1c03be2a36f1ff1a3f19cc53ff5741b81a31a1e2a95a4f8aed90c5fe4",
-         intel: "3dd7c12679fafa1243a6a1920df958fcfea196a3ba334546d7982140cd227073"
+  version "1.15.26,desktop-beta-1.15.26"
+  sha256 arm: "63b6f80522a50463bec9f5e4399a4ea9caa37c12f68fe1d63f03215b3756a72e",
+         intel: "757d018e5f1b1937e6fead9a610bbdb58ced61b357d7d4edd956e27128216c23"
 
   url "https://github.com/BharatCode-ai/bharatcode-desktop/releases/download/#{version.csv.second}/bharatcode-desktop-next-beta-mac-#{arch}.zip",
       verified: "github.com/BharatCode-ai/bharatcode-desktop/"
